146c76b0d23a67fa23261a90b7fe6f4d0391510a
EDZA09 <eduarstiven2001@gmail.com>
2023-11-30 21:21:52 -0500

    Style(Formularios.jsx): Agrego el campo Sintomas en el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -63,6 +63,19 @@ const Formulario = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           />
         </div>
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Sintomas
+          </label>
+          <textarea
+            id="sintomas"
+            placeholder="Describe los Síntomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400"
+          />
+        </div>
       </form>
     </div>
   );
