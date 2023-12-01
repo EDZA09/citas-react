fcce9486c7792131569fa874f9a0dc29328f0dd7
EDZA09 <eduarstiven2001@gmail.com>
2023-11-30 22:26:59 -0500

    Style(ListadoPacientes.jsx): Aplico clases para que el listado sea responsivo

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -2,7 +2,7 @@ import { Paciente } from "./Paciente";
 
 const ListadoPacientes = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado de Pacientes </h2>
 
       <p className="text-xl mt-5 mb-10 text-center">
@@ -10,7 +10,6 @@ const ListadoPacientes = () => {
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
 
-      <Paciente />
       <Paciente />
       <Paciente />
       <Paciente />
