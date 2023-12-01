84aa1438eb52bf86658f6f5a2c786d1e37058dd5
EDZA09 <eduarstiven2001@gmail.com>
2023-11-30 21:19:37 -0500

    Style(Formularios.jsx): Agrega el campo email del Propietario en el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -36,6 +36,20 @@ const Formulario = () => {
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           />
         </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email Contacto Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
       </form>
     </div>
   );
