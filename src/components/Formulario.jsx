9dac5acba5bf39af40a43e3c0b54084c3792155c
EDZA09 <eduarstiven2001@gmail.com>
2023-12-03 22:58:47 -0500

    Feat(Formulario): Agrego el evento de React onSubmit y su funcion receptora

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,6 +3,11 @@ import { useState, useEffect } from "react";
 const Formulario = () => {
   const [nombre, setNombre] = useState(""); //*Manera correcta
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando Formulario");
+  };
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -11,7 +16,10 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
         <div className="mb-5">
           <label
             htmlFor="mascota"
