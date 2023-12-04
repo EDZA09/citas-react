59ce4a41507ccb62bfead7a844836bfd9409acca
EDZA09 <eduarstiven2001@gmail.com>
2023-12-03 23:20:47 -0500

    Feat(Formulario): Valido el valor de los campos del formulario para evitar valores vacíos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,6 +10,13 @@ const Formulario = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Enviando Formulario");
+
+    //*Validación del formulario
+    if ([nombre, propietario, email, fecha, sintomas].includes("")) {
+      console.log("Hay al menos un campo vacío");
+    } else {
+      console.log("Todos llenos");
+    }
   };
 
   return (
