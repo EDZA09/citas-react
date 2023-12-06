74b16f8f8d512660095c59c509d5003de1133395
EDZA09 <eduarstiven2001@gmail.com>
2023-12-06 12:58:28 -0500

    Clean(citas-react): Elimino fragmentos de códigos innecesarios, Por segunda vez.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-//import { useState } from "react";
-/*import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";*/
 import { useState } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
@@ -11,13 +7,9 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   const [pacientes, setPacientes] = useState([]);
 
-  const toma1Valor = (valor) => {
-    console.log(valor);
-  };
-
   return (
     <div className="container mx-auto mt-20">
-      <Header toma1Valor={toma1Valor} />
+      <Header />
       <div className="mt-12 md:flex">
         <Formulario />
         <ListadoPacientes />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,4 @@
-function Header({ toma1Valor }) {
-  const variableHeader = true;
-
-  toma1Valor(variableHeader);
-
+function Header() {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
