1c39173705ad18853e0ca93108928640a08c4c70
EDZA09 <eduarstiven2001@gmail.com>
2023-12-06 12:46:39 -0500

    Clean(citas-react): Elimino comentarios y fragmentos de códigos innecesarios.

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,11 @@ Formulario;
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-  //const [count, setCount] = useState(0);
-  const sumar = () => {
-    //console.log(2 + 2);
-    const numero = 5;
-
-    if (numero > 5) {
-      console.log("Si es mayor");
-    } else {
-      console.log("No es mayor");
-    }
-  };
-
-  //sumar();*/
-
-  //const edad = 18;
   const [pacientes, setPacientes] = useState([]);
 
   return (
     <div className="container mx-auto mt-20">
-      <Header numeros={1} isAdmin={false} fn={sumar} />
+      <Header />
       <div className="mt-12 md:flex">
         <Formulario />
         <ListadoPacientes />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,4 @@
-function Header({ numeros, isAdmin, fn }) {
-  console.log(numeros);
-  console.log(isAdmin);
-  console.log(fn);
+function Header() {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
