310f31aba64a9f141cad936036b132a5397c8fcc
EDZA09 <eduarstiven2001@gmail.com>
2023-12-07 13:12:10 -0500

    Style(Error): Creo un mensaje de error, estático.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,9 @@
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-5 rounded-md">
+      <p>Todos los Campos son Obligatorios</p>
+    </div>
+  );
 };
 
 export default Error;
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 const Formulario = ({ pacientes, setPacientes }) => {
   const [nombre, setNombre] = useState(""); //*Manera correcta
@@ -55,11 +56,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error && (
-          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-5 rounded-md">
-            <p>Todos los Campos son Obligatorios</p>
-          </div>
-        )}
+        {error && <Error />}
         <div className="mb-5">
           <label
             htmlFor="mascota"
