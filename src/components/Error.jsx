2d53a7eb8e401873a9e94b170fd29650e5a668f8
EDZA09 <eduarstiven2001@gmail.com>
2023-12-11 16:12:47 -0500

    Refactor(props): Cambio los props normales por children en los componentes Error y Formulario

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
-const Error = ({ mensaje }) => {
+//const Error = ({ mensaje }) => {
+const Error = ({ children }) => {
+  //toma todo lo que se le envia al componente
   return (
     <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-5 rounded-md">
-      <p>{mensaje}</p>
+      <p>{children}</p>
     </div>
   );
 };
