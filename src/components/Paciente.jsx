84a02662b0e7b7f36d6a4d655866c9c38e58e776
EDZA09 <eduarstiven2001@gmail.com>
2023-12-13 18:44:10 -0500

    feat(Paciente): Utilizo el evento onClick para poder editar el obtejo paciente.

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -28,6 +28,7 @@ export const Paciente = ({ paciente, setPaciente }) => {
         <button
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => setPaciente(paciente)}
         >
           Editar
         </button>
