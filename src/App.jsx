30445327c6c259a88a69d8575d3d6bb8ef8b4765
EDZA09 <eduarstiven2001@gmail.com>
2023-12-13 17:44:35 -0500

    feat(App): Creo la variable de estado paciente

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   const [pacientes, setPacientes] = useState([]);
-
+  const [paciente, setPaciente] = useState({});
   return (
     <div className="container mx-auto mt-20">
       <Header />
