0d0b98ebe2ec37a4146dda971c728ba33d214c64
EDZA09 <eduarstiven2001@gmail.com>
2023-12-26 22:42:34 -0500

    Feat(eliminarPaciente): Agrego el evento onClick para ejecutar la función eliminarPaciente

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -6,7 +6,7 @@ export const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
     console.log("El componente está listo");
   }, []);*/
 
-  const { nombre, propietario, email, fecha, sintomas } = paciente;
+  const { nombre, propietario, email, fecha, sintomas, id } = paciente;
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -41,6 +41,7 @@ export const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
         <button
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+          onClick={() => eliminarPaciente(id)}
         >
           Eliminar
         </button>
